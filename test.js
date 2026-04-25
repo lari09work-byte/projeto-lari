02cd3a9d05b0499e50c336226a5d42ae2902ea1f
lari09work-byte <lari09work@gmail.com>
2026-04-25 19:00:54 -0300

    test.js
    teste

--- test.js
+++ test.js
@@ -2,22 +2,22 @@ function soma(a, b) {
   return a + b;
 }
 
-test('soma 1 + 1 = 2', () => {
+test('teste 1', () => {
   expect(soma(1, 1)).toBe(2);
 });
 
-test('soma 2 + 2 = 4', () => {
+test('teste 2', () => {
   expect(soma(2, 2)).toBe(4);
 });
 
-test('soma 5 + 5 = 10', () => {
+test('teste 3', () => {
+  expect(soma(3, 3)).toBe(6);
+});
+
+test('teste 4', () => {
+  expect(soma(4, 4)).toBe(8);
+});
+
+test('teste 5', () => {
   expect(soma(5, 5)).toBe(10);
-});
-
-test('soma 0 + 0 = 0', () => {
-  expect(soma(0, 0)).toBe(0);
-});
-
-test('soma 10 + 5 = 15', () => {
-  expect(soma(10, 5)).toBe(15);
 });
